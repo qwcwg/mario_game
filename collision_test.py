bf71010f9a465f75451568ae8bb15bc3a9fcca9a
none <none>
2023-07-30 21:51:47 +0900

    1.0.4 collide_testing_1

--- collision_test.py
+++ collision_test.py
@@ -32,6 +32,10 @@ class Sprite(pygame.sprite.Sprite):  # Sprite 클래스
     self.to_x = 0
     self.to_y = 0
 
+    self.mask = pygame.mask.from_surface(self.image)
+
+    self.rect_test = self.image.get_rect()
+    self.rect = pygame.Rect(x_pos, y_pos, self.width, self.hight)  # Rect
 
   def draw(self): # 그리기 method
     screen.blit(self.image, (self.x_pos, self.y_pos))
@@ -39,8 +43,23 @@ class Sprite(pygame.sprite.Sprite):  # Sprite 클래스
 
 
 class Player(Sprite):   # Player 클래스. 점프 기능 추가
+
   def __init__(self, image_path, x_pos, y_pos):
     super().__init__(image_path, x_pos, y_pos)
+
+    
+    self.image = pygame.image.load(image_path)
+
+    self.width = self.image.get_rect().size[0]
+    self.hight = self.image.get_rect().size[1]
+    self.x_pos = x_pos
+    self.y_pos = y_pos
+    self.to_x = 0
+    self.to_y = 0
+
+    self.rect = pygame.Rect(x_pos, y_pos, self.width, self.hight)  # Rect
+
+    self.rect_test = self.image.get_rect()
 
     self.jumping = False
     self.jump_hight = 15
@@ -49,7 +68,7 @@ class Player(Sprite):   # Player 클래스. 점프 기능 추가
 
     self.fall_count = 1
 
-  def move_right(self):   # 이동 method
+  def move_right(self):
     self.to_x += self.speed
   def move_left(self):
     self.to_x -= self.speed
@@ -60,7 +79,7 @@ class Player(Sprite):   # Player 클래스. 점프 기능 추가
     self.x_pos += self.to_x
     self.y_pos += self.to_y
 
-  def gravity(self):  # 중력 설정
+  def gravity(self):
     self.to_y += min(1, (self.fall_count / FPS) * GRAVITY)
     self.fall_count += 1
     
@@ -70,7 +89,7 @@ class Player(Sprite):   # Player 클래스. 점프 기능 추가
     keys = pygame.key.get_pressed()
 
     self.to_x = 0
-    if keys[pygame.K_RIGHT]:  # 키를 눌렀을 때
+    if keys[pygame.K_RIGHT]:
       self.move_right()
     if keys[pygame.K_LEFT]:
       self.move_left()
@@ -84,14 +103,20 @@ class Player(Sprite):   # Player 클래스. 점프 기능 추가
       self.jumping = False
       self.y_velocity = self.jump_hight
     
-    # self.gravity()    # 중력 함수 사용
+    self.gravity()
 
     self.x_pos += self.to_x
     self.y_pos += self.to_y
   
 
+def handle_collision(player, block):
+  if pygame.sprite.collide_mask(player, block):
+    player.rect.bottom = block.rect.top
 
-
+# def handle_collision(player, block):    # 나도코딩 방식
+#   if player.rect_test.colliderect(block.rect_test):
+#     print("충돌")
+#     player.rect.bottom = block.rect.top
 
 
 
@@ -102,14 +127,19 @@ ground = Sprite(ground_path, 0, 800)
 
 running = True
 while running:
-  dt = clock.tick(60)
+  clock.tick(FPS)
+
   for event in pygame.event.get():
 
     if event.type == pygame.QUIT:
       running = False
+      print(player.rect.bottom)
 
 
   player.handle_move()
+  handle_collision(player, ground)
+
+
 
 
   background.draw()  # 화면에 그리기
@@ -118,7 +148,7 @@ while running:
 
   pygame.display.update()
 
-print(player.image.get_rect())
+
 
 
 
