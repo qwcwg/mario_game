433d0a4b31cd9bb4e75149c5d96058a6374d348d
none <none>
2023-08-13 00:58:16 +0900

    1.0.9 - jump_test_1

--- jump.py
+++ jump.py
@@ -8,7 +8,6 @@ screen_height = 900
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 FPS = 60
-global GRAVITY
 GRAVITY = 1
 
 # 파일 Path 설정
@@ -44,6 +43,7 @@ class Sprite(pygame.sprite.Sprite):  # Sprite 클래스
     self.bottom_y = self.y_pos + 50
     self.to_x = 0
     self.to_y = 0
+    self.gravity = 1
 
   def draw(self):  # 그리기 method
     screen.blit(self.image, (self.x_pos, self.y_pos))
@@ -73,14 +73,22 @@ class Player(Sprite):  # Player 클래스. 점프 기능 추가
     self.y_pos += self.to_y
 
 def jump_and_gravity(player):  # 중력 설정
-  GRAVITY = 1
-  player.to_y += GRAVITY
-  GRAVITY += 1
+  player.gravity = 1
+  player.to_y += player.gravity # 아래로 떨어짐
+  player.gravity += 1 
   keys = pygame.key.get_pressed()
   if player.bottom_y >= 800:
     player.to_y = 0
+  if player.to_y == 0 :
     if keys[pygame.K_SPACE]:
       player.to_y = -18
+
+def top_collision(player, block):  # 블럭을 플레이어가 밟을 때 충돌
+  if block.top_y <= player.bottom_y and player.to_y > 0 :  # 플레이어가 블록의 사이(상하)에 있을 때
+    if  block.left_x < player.left_x < block.right_x  or  block.left_x < player.right_x < block.right_x: # 플레이어가 블록의 사이(좌우)에 있을 때
+      player.to_y = 0
+      player.y_pos = block.top_y + player.height
+  
     
 
 
@@ -93,26 +101,14 @@ def handle_move(player):
     player.move_right()
   if keys[pygame.K_LEFT]:
     player.move_left()
-  # if keys[pygame.K_SPACE] and player.jumping == False:
-  #   player.move_jump()    
-  # if keys[pygame.K_r]:
-  #   weapon.active = True
 
-
-  # if player.jumping == True:  # 점프 설정(위로 올라가기만, 떨어지는건 중력이)
-  #   player.to_y -= player.y_velocity
-  #   if player.y_velocity >= 0:
-  #     player.y_velocity -= 1
-  #     player.jumping = False
-
-  # if player.jumping == False:
   jump_and_gravity(player)  # 중력&점프 함수 사용
 
 
   player.x_pos += player.to_x
   player.y_pos += player.to_y
 
-
+ 
 def pos_update(player):  #  위치 초기화 함수
 
   player.point_1 = [player.x_pos, player.y_pos]
@@ -129,6 +125,7 @@ def pos_update(player):  #  위치 초기화 함수
 player = Player(player_path, 100, 750)  # 클래스 생성
 background = Sprite(background_path, 0, 0)
 ground = Sprite(ground_path, 0, 800)
+block_1 = Sprite(block_1_path, 200, 650)
 
 
 running = True
@@ -139,13 +136,15 @@ while running:
     if event.type == pygame.QUIT:
       running = False
 
+
+  top_collision(player, block_1)
   handle_move(player)
   pos_update(player)  # 위치 업데이트 함수
 
   background.draw()  # 화면에 그리기
   ground.draw()
   player.draw()
-  # screen.blit(player.image, (player.point_1[0], player.point_1[1]))
+  block_1.draw()
 
   pygame.display.update()
 
